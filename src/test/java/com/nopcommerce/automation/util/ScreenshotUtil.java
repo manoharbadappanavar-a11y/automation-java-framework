40ef7318ceb2b1489a9bcc231a5cf562ce982769
manoharbadappanavar-a11y <manoharbadappanavara@gmail.com>
2025-09-24 17:04:49 +0530

    Added timestamp to avoid overwriting screenshots

--- src/test/java/com/nopcommerce/automation/util/ScreenshotUtil.java
+++ src/test/java/com/nopcommerce/automation/util/ScreenshotUtil.java
@@ -1,23 +1,38 @@
 package com.nopcommerce.automation.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 public class ScreenshotUtil {
 
     public static String captureScreenshot(String screenshotName) {
-        WebDriver driver = DriverManager.getDriver(); // get the driver from DriverManager
+        WebDriver driver = DriverManager.getDriver();
+
         try {
+            // Take screenshot
             File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            String dest = System.getProperty("user.dir") + "/test-output/screenshots/" + screenshotName + ".png";
+
+            // Add timestamp to avoid overwriting
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+            String dest = System.getProperty("user.dir") +
+                    "/test-output/screenshots/" + screenshotName + "_" + timestamp + ".png";
+
             File target = new File(dest);
+
+            // Ensure directory exists
             Files.createDirectories(target.getParentFile().toPath());
+
+            // Copy screenshot file
             Files.copy(src.toPath(), target.toPath());
+
             return dest;
         } catch (IOException e) {
             e.printStackTrace();
